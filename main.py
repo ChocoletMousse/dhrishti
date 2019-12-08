fd74439d1a9b7c380f97c8b49cecae40333bad88
Karan Nagaraj <karan_95@hotmail.co.uk>
2019-12-08 23:23:43 +0000

    connector working

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from settings import Setup
+from sources.reddit import RedditConnector
 
 
 def main():
-    setup = Setup()
-    print(f"client id: {setup.client_id}")
+    reddit_connector = RedditConnector()
+    reddit = reddit_connector.get_reddit_connector()
+    subreddit = reddit.subreddit('python').hot(limit=10)
+
+    for submission in subreddit:
+        print(f"title: {submission.title}")
 
 
 if __name__ == "__main__":
